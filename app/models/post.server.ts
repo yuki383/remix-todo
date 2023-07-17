3eb2029597b33957df59864afd41a9ebba6f08dd
yuki383 <yuukikokubo.pg@gmail.com>
2023-07-17 18:29:09 +0900

    add posts..tsx

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -3,10 +3,19 @@ import type { AppLoadContext } from "@remix-run/server-runtime";
 type Post = {
   slug: string;
   title: string;
+  markdown: string;
 };
 
 export async function getPosts(ctx: AppLoadContext): Promise<Array<Post>> {
   const res = await ctx.env.CFDB.prepare("select * from posts").all();
 
   return res.results as Post[];
+}
+
+export async function getPost(ctx: AppLoadContext, slug: string): Promise<Post> {
+  const result = await ctx.env.CFDB.prepare("select * from posts where slug = ?")
+    .bind(slug)
+    .first();
+
+  return result as Post;
 }
---
+++ app/routes/posts.$slug.tsx
@@ -0,0 +1,33 @@
+import { json, type LoaderFunction } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+import { marked } from "marked";
+import { getPost } from "../models/post.server";
+
+export const loader = (async ({ params, context }) => {
+  if (!params.slug) {
+    throw new Error("params.slug is required");
+  }
+
+  const post = await getPost(context, params.slug);
+  if (!post) {
+    throw new Error(`Post not found: ${params.slug}`);
+  }
+
+  const html = marked(post.markdown);
+
+  return json({
+    post,
+    html,
+  });
+}) satisfies LoaderFunction;
+
+export default function PostSlug() {
+  const { post, html } = useLoaderData<typeof loader>();
+  console.log(post);
+  return (
+    <main>
+      <h1>Some Post: {post.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </main>
+  );
+}
