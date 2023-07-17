5613e428c235a940e32c994a43d2c5b5618bfced
yuki383 <yuukikokubo.pg@gmail.com>
2023-07-17 19:00:09 +0900

    Implements the creation of a post

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -12,10 +12,23 @@ export async function getPosts(ctx: AppLoadContext): Promise<Array<Post>> {
   return res.results as Post[];
 }
 
-export async function getPost(ctx: AppLoadContext, slug: string): Promise<Post> {
-  const result = await ctx.env.CFDB.prepare("select * from posts where slug = ?")
+export async function getPost(
+  ctx: AppLoadContext,
+  slug: string
+): Promise<Post> {
+  const result = await ctx.env.CFDB.prepare(
+    "select * from posts where slug = ?"
+  )
     .bind(slug)
     .first();
 
   return result as Post;
+}
+
+export async function createPost(ctx: AppLoadContext, post: Post) {
+  await ctx.env.CFDB.prepare(
+    "insert into posts (slug, title, markdown) values (?, ?, ?)"
+  )
+    .bind(post.slug, post.title, post.markdown)
+    .run();
 }
--- app/routes/posts.admin.new.tsx
+++ app/routes/posts.admin.new.tsx
@@ -1,3 +1,71 @@
+import { Form, useActionData, useNavigation } from "@remix-run/react";
+import { redirect, type ActionFunction, json } from "@remix-run/cloudflare";
+import { createPost } from "../models/post.server";
+
+export const action = (async ({ request, context }) => {
+  const formData = await request.formData();
+
+  const title = formData.get("title");
+  const slug = formData.get("slug");
+  const markdown = formData.get("markdown");
+
+  const errors = {
+    title: title ? null : "Title is required",
+    slug: slug ? null : "Slug is required",
+    markdown: markdown ? null : "Markdown is required",
+  };
+  const hasErrors = Object.values(errors).some((error) => error !== null);
+  if (hasErrors) {
+    return json(errors);
+  }
+
+  if (typeof title !== "string") {
+    throw new Error("title must be a string");
+  }
+  if (typeof slug !== "string") {
+    throw new Error("slug must be a string");
+  }
+  if (typeof markdown !== "string") {
+    throw new Error("markdown must be a string");
+  }
+
+  await createPost(context, { title, slug, markdown });
+
+  return redirect("/posts/admin");
+}) satisfies ActionFunction;
+
 export default function NewPost() {
-  return <h2>New Post</h2>;
+  const errors = useActionData<typeof action>();
+
+  const navigation = useNavigation();
+  const isCreating = navigation.state === "submitting";
+
+  return (
+    <Form method="post">
+      <p>
+        <label>
+          Post Title: {errors?.title && <em>{errors.title}</em>}
+          <input type="text" name="title" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Post Slug: {errors?.slug && <em>{errors.slug}</em>}
+          <input type="text" name="slug" />
+        </label>
+      </p>
+      <p>
+        <label htmlFor="markdown">
+          Markdown: {errors?.markdown && <em>{errors.markdown}</em>}
+        </label>
+        <br />
+        <textarea id="markdown" rows={20} name="markdown" />
+      </p>
+      <p>
+        <button type="submit" disabled={isCreating}>
+          {isCreating ? "Creating..." : "Create Post"}
+        </button>
+      </p>
+    </Form>
+  );
 }
